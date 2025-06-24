b7ca96dfc6d343281eb7f2730aef269a6758fc92
Lucenith Pinilla Moreno <167031042+LucenithGitHub@users.noreply.github.com>
2025-06-24 10:52:03 -0500

    Ajuste 2daVal Andrea 24.06 via teams

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -283,7 +283,7 @@
     .row.justify-content-center.align-items-center.mb-4(data-aos="fade")
       .titulo-sexto.color-acento-contenido
         h5 Tabla 1. 
-        | Las proporciones y sus funciones
+        | Las preposiciones y sus funciones
       figure
         img.mb-2(src='@/assets/curso/t2/tabla1.png', alt='Se detallan las funciones semánticas más comunes de las preposiciones, acompañadas de algunos ejemplos ilustrativos.')
       figcaption Referencia Tabla - Las proporciones y sus funciones EAFIT (s.f.)
